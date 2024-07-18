2aaae6519e19546df28a794f48cee198e4d223e1
TheFirstRule <110130956+TheFirstRule@users.noreply.github.com>
2024-07-18 14:09:10 +0300

    Delete Dictionary directory

--- Dictionary/src/main/java/Yashchenko/Dictionary/repository/DataRepository.java
+++
@@ -1,9 +0,0 @@
-package Yashchenko.Dictionary.repository;
-
-import Yashchenko.Dictionary.model.DataItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface DataRepository extends JpaRepository<DataItem, UUID> {
-}
